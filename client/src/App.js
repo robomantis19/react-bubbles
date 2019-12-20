0904dc53643c199148193656e61db7a21a403843
robomantis19 <freefavors@gmail.com>
2019-12-20 12:02:19 -0500

    login page and get /api/colors working!

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import { BrowserRouter as Router,  Link, Route, Switch } from "react-router-dom";
+import BubblePage from './components/BubblePage'; 
 import Login from "./components/Login";
 import "./styles.scss";
 
@@ -8,11 +8,15 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        
+        <div style={{display: `flex`, width: `10%`, height: `100px`}}>
+          <Link to="/login">Login</Link>
+          <Link to="/BubblePage">Home</Link>
+        </div>
+        <Switch>
+          <Route path='/login' component={Login}/>
+          <Route exact path="/BubblePage" component={BubblePage}/>
+        </Switch>
       </div>
     </Router>
   );
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
-const Login = () => {
+
+const Login = (props) => {
+  const [credentials, setCredentials] = useState({username: "", password: ""})
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const login = e => { 
+    e.preventDefault()
+    axiosWithAuth()
+      .post('/login', credentials)
+      .then(res => { 
+        console.log('response', res)
+        localStorage.setItem('token', res.data.payload);
+        props.history.push('/BubblePage');
+      
+      })
+  }
+  const handleChange = (e) => {
+    setCredentials({...credentials, [e.target.name]: e.target.value}); 
+  }
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+    <>      <div>
+              <h2 style={{color: 'darkGrey', fontWeight: "bold", marginLeft: `300px`}}>Login</h2>
+               
+               <form onSubmit={login} style= {{zIndex: `100`, backgroundColor: `darkOrange`, position: `relative`, left: `200px`, height: `250px`, width: `200px`, display: `flex`, flexDirection: `column`, padding: `35px`}}>
+                   <label>Username</label>
+                   <input style={{margin: `auto`, width: `120px`}}
+                   type = "text"
+                   name = "username" 
+                   value = {credentials.username}
+                   onChange = {handleChange}
+                   /> 
+                   <label>Password</label>
+                   <input style={{margin: `auto`, width: `120px`}}
+                   type = "text" 
+                   name = "password" 
+                   value = {credentials.password}
+                   onChange = {handleChange}
+                   /> 
+                   <button style={{margin: `auto`, width: `80px`, height: `30px`}} >Log in</button>
+                   
+               </form>
+            </div>
     </>
   );
 };
